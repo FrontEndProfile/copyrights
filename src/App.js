731940333b44c246a38ac8072e3ba2903b290f63
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-03-02 11:21:50 +0500

    create pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+
+        {/* <div>
+          <ul>
+            <li>
+              <Link className="nav-link" to='/'>Home</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to='/Profile'>Profile</Link>
+
+            </li>
+          </ul>
+        </div> */}
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Header from '../../components/header/Header'
+const home = () => {
+  return (
+    <>
+    <Header/>
+    <h1>home</h1>
+
+    </>
+  )
+}
+
+export default home
