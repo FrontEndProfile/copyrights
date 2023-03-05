0f204d1b4d3216a7c9c1d75b4745c7118a5f4a36
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-03-05 16:04:28 +0500

    profile page starting

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -4,21 +4,26 @@ import './profile.css'
 
 import Lines from '../../assets/images/circles.png'
 import heroImage from '../../assets/images/hero-img.png'
+import { Link } from 'react-router-dom'
 
 const Profile = () => {
   return (
     <>
       <div className='viewPage'>
         <Header />
-        <section className='HeroComponent'>
+        <section className='HeroComponent dataPage'>
           <div className='heroLines'>
             <img className='img-fluid mw-100' src={Lines} alt='' />
           </div>
 
           <div className='container-xl position-relative'>
-            <div className='row HeroContent'>
-              <div className='col-md-12'>
-              
+            <div className='row profileRow'>
+              <div className='col-md-6'>
+                <h1>Profile</h1>
+                <p>More than 400+ new members</p>
+              </div>
+              <div className='col-md-6'>
+                <Link to='#' className='ui_btn addProfileLayout'>Add Profiles</Link>
               </div>
             </div>
           </div>
